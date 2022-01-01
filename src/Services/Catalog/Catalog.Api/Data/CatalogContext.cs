464b7c282d666d1d119216ccce5be55b2421c393
Islam Helmy <IslamHelmy@Desktop>
2022-01-01 02:00:02 +0200

    Create Catalog Context

---
+++ src/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}

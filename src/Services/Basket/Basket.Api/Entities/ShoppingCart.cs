2bc82562e0a58a7b8fbb2383ba83800205c4cc35
Islam Helmy <IslamHelmy@Desktop>
2022-01-01 23:42:44 +0200

    Add Basket Entities

---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart()
+        {
+
+        }
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; } = new();
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalPrice = 0;
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+                return totalPrice;
+            }
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.Api/Entities/ShoppingCartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}

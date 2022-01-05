020ea2ee2c497b1d7ffc02ab2a684f3ec1db1447
Islam Helmy <IslamHelmy@Desktop>
2022-01-05 04:26:14 +0200

    Add Discount Controller

---
+++ src/Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,52 @@
+﻿using Discount.Api.Entities;
+using Discount.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Discount.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository ?? throw new ArgumentNullException(nameof(discountRepository));
+        }
+
+        [HttpGet("{productName}", Name = nameof(GetDiscount))]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var discount = await _discountRepository.GetDiscount(productName);
+            return Ok(discount);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> CreateDiscount(Coupon coupon)
+        {
+            await _discountRepository.CreateDiscount(coupon);
+            var discount = await _discountRepository.GetDiscount(coupon.ProductName);
+            return Ok(discount);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> UpdateDiscount(Coupon coupon)
+        {
+            return Ok(await _discountRepository.UpdateDiscount(coupon));
+        }
+
+        [HttpDelete("{productName}", Name = nameof(DeleteDiscount))]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            return Ok(await _discountRepository.DeleteDiscount(productName));
+        }
+    }
+}

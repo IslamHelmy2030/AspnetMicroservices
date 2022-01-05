adba42faa57fffe5f62cc73812ad4946f898b7a6
Islam Helmy <IslamHelmy@Desktop>
2022-01-05 03:48:44 +0200

    Add Discount Repository

---
+++ src/Services/Discount/Discount.Api/Repositories/IDiscountRepository.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Api.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Api.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}

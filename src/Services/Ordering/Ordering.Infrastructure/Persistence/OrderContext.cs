4275fef7b0fc3ab395545885dbf852252f031b9d
Islam Helmy <IslamHelmy@Desktop>
2022-01-15 11:45:11 +0200

    Add OrderContext

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedBy = "Islam Helmy";
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        break;
+                    case EntityState.Added:
+                        entry.Entity.CreatedBy = "Islam Helmy";
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        break;
+                }
+            } 
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}

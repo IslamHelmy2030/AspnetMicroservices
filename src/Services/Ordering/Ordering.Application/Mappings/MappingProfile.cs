25e15fd99fdd5f322500dceeca9e034f9d442061
Islam Helmy <IslamHelmy@Desktop>
2022-01-15 02:41:21 +0200

    Add UpdateOrderCommand and NotFoundException

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Ordering.Application.Features.Orders.Commands.CheckOutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries;
 using Ordering.Domain.Entities;
 
@@ -11,6 +12,7 @@ namespace Ordering.Application.Mappings
         {
             CreateMap<Order, OrdersVm>().ReverseMap();
             CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }

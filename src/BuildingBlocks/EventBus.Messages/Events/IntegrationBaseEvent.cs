b8178db3875d1d4235b8e3ed5dabc11598ed4fdb
Islam Helmy <IslamHelmy@Desktop>
2022-01-17 23:22:03 +0200

    Add IntegrationBaseEvent Class

---
+++ src/BuildingBlocks/EventBus.Messages/Events/IntegrationBaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EventBus.Messages.Events
+{
+    public class IntegrationBaseEvent
+    {
+        public IntegrationBaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreateDate = DateTime.UtcNow;
+        }
+
+        public IntegrationBaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreateDate = createDate;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime CreateDate { get; private set; }
+    }
+}

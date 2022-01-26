ba50fbdafcdd7cd493861ffe5409fbe9ed5611fd
Islam Helmy <IslamHelmy@Desktop>
2022-01-27 00:28:19 +0200

    Add Ocelot

--- src/ApiGateways/OcelotApiGw/Startup.cs
+++ src/ApiGateways/OcelotApiGw/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +18,11 @@ namespace OcelotApiGw
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async Task ConfigureAsync(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +38,8 @@ namespace OcelotApiGw
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            await app.UseOcelot();
         }
     }
 }

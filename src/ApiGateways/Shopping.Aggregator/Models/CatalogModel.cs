95a641922e90a66507532832b9628987e86ad844
Islam Helmy <IslamHelmy@Desktop>
2022-01-29 23:52:56 +0200

    Add BasketModel

--- src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shopping.Aggregator.Models
+﻿namespace Shopping.Aggregator.Models
 {
     public class CatalogModel
     {

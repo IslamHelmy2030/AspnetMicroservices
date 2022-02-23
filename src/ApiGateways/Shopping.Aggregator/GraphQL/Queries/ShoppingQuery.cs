f1cf52f46c642f6b1eaad745c27c8692b4a226fe
Islam Helmy <IslamHelmy@Desktop>
2022-02-23 09:09:05 +0200

    GraphQL Enhancement

--- src/ApiGateways/Shopping.Aggregator/GraphQL/Queries/ShoppingQuery.cs
+++ src/ApiGateways/Shopping.Aggregator/GraphQL/Queries/ShoppingQuery.cs
@@ -8,41 +8,20 @@ namespace Shopping.Aggregator.GraphQL.Queries
 {
     public class ShoppingQuery : ObjectGraphType
     {
-        private readonly IShoppingRepository _shoppingRepository;
         public ShoppingQuery(IShoppingRepository shoppingRepository)
         {
-            _shoppingRepository = shoppingRepository;
-
             // Fields
-            FieldAsync<BasketType>(
-               name: "basket",
-               description: "Basket With Products",
+            FieldAsync<ShoppingType>(
+               name: "shopping",
+               description: "Basket With Products and Orders",
                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "username" }),
                resolve: async context =>
                {
                    try
                    {
                        string userName = context.GetArgument<string>("username");
-                       var basket = await _shoppingRepository.GetBasket(userName);
-                       return basket;
-                   }
-                   catch (Exception ex)
-                   {
-                       throw new ExecutionError(ex.Message);
-                   }
-               });
-
-            FieldAsync<ListGraphType<OrderResponseType>>(
-                name: "orders",
-                description: "Orders",
-                arguments: new QueryArguments(new QueryArgument<NonNullGraphType<StringGraphType>> { Name = "username" }),
-               resolve: async context =>
-               {
-                   try
-                   {
-                       string userName = context.GetArgument<string>("username");
-                       var basket = await _shoppingRepository.GetOrders(userName);
-                       return basket;
+                       var shopping = await shoppingRepository.GetShopping(userName);
+                       return shopping;
                    }
                    catch (Exception ex)
                    {
---
+++ src/ApiGateways/Shopping.Aggregator/GraphQL/Types/ShoppingType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.GraphQL.Types
+{
+    public class ShoppingType: ObjectGraphType<ShoppingModel>
+    {
+        public ShoppingType()
+        {
+            Field(f => f.BasketWithProducts, type: typeof(BasketType)).Description("Basket With Products");
+            Field(f => f.Orders, type: typeof(ListGraphType<OrderResponseType>)).Description("Orders");
+        }
+    }
+}
